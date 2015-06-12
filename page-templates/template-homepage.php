d23ea0528f7578e8254dd0d4af4850112a3fe683
Janine Hempy <jhempy@gmail.com>
2015-06-12 11:06:52 -0400

    Added homepage template

---
+++ page-templates/template-homepage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Homepage
+ *
+ * This template display content at full with, with no sidebars.
+ * Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site will use a different template.
+ *
+ * @package berea
+ */
+
+get_header(); ?>
+
+        <div id="primary" class="content-area">
+                <main id="main" class="site-main" role="main">
+
+
+This is the homepage.
+
+
+                </main><!-- #main -->
+        </div><!-- #primary -->
+
+<?php get_footer(); ?>
