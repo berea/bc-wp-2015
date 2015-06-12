1126e15a1e24e7b5dfa29b761ba4d8eccfe8a502
Janine Hempy <jhempy@gmail.com>
2015-06-12 15:40:04 -0400

    Added dummy content to footer and homepage includes

--- footer.php
+++ footer.php
@@ -19,12 +19,46 @@
 			<?php tha_footer_top(); ?>
 
 
+<h3>NO-TUITION PROMISE</h3>
+
+<p>Berea is the only one of America’s top colleges that awards every enrolled student a no-tuition promise.</p>
+
+<p>No student pays for tuition.</p>
+
+<p>Our generous TUTITION PROMISE SCOLARSHIP makes it possible for you to graduate debt-free. Even if you borrow for special learning opportunites or to replace your family’s total contribution, you will have a low debt compared to national trends. We sometimes call Berea “the best education money can’t buy.”</p>
+
+<h3>ABOUT BEREA</h3>
+
+<ul>
+	<li>Our Mission</li>
+	<li>Visitor’s Center</li>
+	<li>Jobs</li>
+</ul>
+
+<ul>
+	<li>Partners for Education</li>
+	<li>Seabury Athletic Center</li>
+	<li>Loyal Jones Appalachian Center</li>
+	<li>bell hooks Institute</li>
+	<li>Hutchins Library</li>
+</ul>
+
+<ul>
+	<li>City of Berea</li>
+	<li>Madison County</li>
+	<li>Kentucky</li>
+</ul>
+
+
+<h3>DISTINCTLY BEREAN</h3>
+
+<h3>MAPS &amp; DIRECTIONS</h3>
+
 
 
 <div id="footer-social">
 	<p>FOLLOW US: Facebook, YouTube, Twitter, Instagram, RSS</p>
 </div><!-- #footer-social -->
-
 
 			<div class="site-info">
 
--- page-templates/partials/news.php
+++ page-templates/partials/news.php
@@ -1,3 +1,26 @@
 <!-- Last 4 news items -->
 
 <h1>NEWS</h1>
+
+<ul>
+	<li>
+		<p>Jan 09, 2015</p>
+		<img src="/wp-content/themes/berea2015/bc-wp-2015/assets/images/221_147_placeholder.jpg">
+		<p>Martin Luther King, Jr. Events at Berea College</p>
+	</li>
+	<li>
+		<p>Jan 08, 2015</p>
+		<img src="/wp-content/themes/berea2015/bc-wp-2015/assets/images/221_147_placeholder.jpg">
+		<p>Berea Named Most Affordable College in America by USA Today</p>
+	</li>
+	<li>
+		<p>Jan 08, 2015</p>
+		<img src="/wp-content/themes/berea2015/bc-wp-2015/assets/images/221_147_placeholder.jpg">
+		<p>Berea College Selected by Carnegie for 2015 Community Engagement Classification</p>
+	</li>
+	<li>
+		<p>Dec 18, 2014</p>
+		<img src="/wp-content/themes/berea2015/bc-wp-2015/assets/images/221_147_placeholder.jpg">
+		<p>Berea College Named a 2015 Best Value College by Kiplinger's Personal Finance</p>
+	</li>
+</ul>
