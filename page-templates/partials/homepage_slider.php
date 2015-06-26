bf1b7e8af9a79bd6b52b978d919c7aacf003f1aa
Janine Hempy <jhempy@gmail.com>
2015-06-26 07:57:07 -0400

    Working on making lists display inline.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -1,18 +1,18 @@
 <!-- This is the homepage slider -->
 
-<div id="headline">
+<div class="headline">
 	<h1>THE BEST EDUCATION MONEY CAN'T BUY</h1>
 </div>
 
-<div id="tagline">
+<div class="tagline">
 	<p>Donec id elit non mi porta gravida at eget metus.</p>
 </div>
 
-<div id="readmore">
+<div class="readmore">
 	<input type="button" value="Read More">
 </div>
 
-<div id="hero-nav">
+<div class="hero-nav">
 	<ul>
 		<li>APPLY</li>
 		<li>GIVE</li>
