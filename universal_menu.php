f13efe8ee36e4cba47a564a202e0e762938d67b6
Ron Northrip <ron.northrip@gmail.com>
2015-08-24 19:12:48 -0400

    Break out universal menu into a separate file to use as the main menu and then add logic to use/repeat that block for mobile injection of menu into hamburger

--- universal_menu.php
+++ universal_menu.php
@@ -1,160 +1,41 @@
 <div class="cd-overlay"></div>
+<?php $berea_should_have_default_menu = berea_has_default_menu(); ?>
 
 <nav class="cd-nav">
-	<ul id="cd-primary-nav" class="cd-primary-nav is-fixed">
-		<li class="has-children">
-			<a href="#0">ADMISSIONS</a>
-
-			<ul class="cd-secondary-nav is-hidden">
-				<li class="go-back"><a href="#0">MENU</a></li>
-				<li class="see-all"><a href="http://dev.berea.edu/subsite/">ADMISSIONS&gt;</a></li>
-				<li class="has-children">
-					<a href="#">ADMISSIONS INFO</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">ADMISSIONS INFO</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Admission Requirements</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Application Forms</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">New Freshmen</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">International Students</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Transfer Students</a></li>
-					</ul>
-				</li>
-				<li class="has-children">
-					<a href="#">STUDENT LIFE</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">STUDENT LIFE</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Career Preparation</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Athletics</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Study Abroad</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Service and Leadership</a></li>
-					</ul>
-				</li>
-				<li class="has-children">
-					<a href="#">FINANCIAL AID</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">FiNANCIAL AID</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Tuition Promise Scholarship</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Labor Grant Aid</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Financial Eligibility</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Estimate First-Year Costs</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">Submit and RFQ</a></li>
-					</ul>
-				</li>
-				<li class="has-children">
-					<a href="#">???</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">???</a></li>
-						<li><a href="http://dev.berea.edu/subsite//">APPLY TODAY</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">CONFIRM YOUR FINANCIAL ELIGIBILITY</a></li>
-						<li><a href="http://dev.berea.edu/subsite/">SCHEDULE A CAMPUS VISIT</a></li>
-					</ul>
-				</li>
+	<ul id="cd-primary-nav" class="cd-primary-nav is-fixed <?php echo ($berea_should_have_default_menu) ? 'apply-mobile-injected-menu': ''; ?>">
+	
+	<?php if ($berea_should_have_default_menu) { ?>
+		<!-- subsite menu with Berea submenu -->
+		<?php 
+			foreach (berea_get_default_menu(true) as $col => $vals) {
+				$please_close_ul=false;
+				if (count($vals)>1) {
+					$please_close_ul=true;
+					$first_val=array_shift($vals);
+					
+					echo '<li class="has-children menu-item-hide-on-large">' . $first_val . 
+						"\n<ul class=\"is-hidden\">\n".
+						'<li class="go-back"><a href="#0">BACK</a></li>';
+				}
+				foreach ($vals as $key => $val) {
+					echo "<li>" . $val . "</li>\n";
+				}
+				if ($please_close_ul) { echo "</li></ul>"; }
+			}
+		?>
+		
+		<li class="has-children menu-item-hide-on-large">
+			<a href="#">BEREA</a>
+			<ul class="is-hidden">
+				<li class="go-back"><a href="#0">BACK</a></li>
+				<?php include('universal_menu_master.php'); ?>
 			</ul>
 		</li>
-
-		<li class="has-children">
-			<a href="#">ACADEMICS</a>
-			<ul class="cd-nav-icons is-hidden">
-				<li class="go-back"><a href="#0">Menu</a></li>
-				<li class="see-all"><a href="#">ACADEMICS&gt;</a></li>
-				<li>
-					<a class="cd-nav-item item-1" href="http://dev.berea.edu/subsite/">
-						<h3>DEGREE PROGRAMS</h3>
-						<p>Bachelor of Arts and Bachelor of Science degrees in 28 fields.</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-2" href="http://dev.berea.edu/subsite/">
-						<h3>LABOR PROGRAM</h3>
-						<p>Students are engaged in a learning environment that encourages them to be active learners, workers, and servers.</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-3" href="http://dev.berea.edu/subsite/">
-						<h3>LIBRARY</h3>
-						<p>It has books. And stuff.</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-4" href="http://dev.berea.edu/subsite/">
-						<h3>MAJORS & MINORS</h3>
-						<p>This is the item description</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-5" href="http://dev.berea.edu/subsite/">
-						<h3>ACADEMIC CALENDAR</h3>
-						<p>This is the item description</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-6" href="http://dev.berea.edu/subsite/">
-						<h3>CENTERS</h3>
-						<p>This is the item description</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-7" href="http://dev.berea.edu/subsite/">
-						<h3>CATALOG & STUDENT HANDBOOK</h3>
-						<p>This is the item description</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-8" href="http://dev.berea.edu/subsite/">
-						<h3>RESOURCES</h3>
-						<p>This is the item description</p>
-					</a>
-				</li>
-
-				<li>
-					<a class="cd-nav-item item-8" href="http://dev.berea.edu/subsite/">
-						<h3>FACULTY & STAFF SCHOLARLY WORK</h3>
-						<p>This is the item description</p>
-					</a>
-				</li>
-			</ul>
-		</li>
-
-		<li class="">
-			<a href="http://www.bereacollegealumni.com/s/925/start.aspx">ALUMNI</a>
-		</li>
-
-		<li class="has-children">
-			<a href="http://community.berea.edu/calendar/index.php?calendar=default">NEWS</a>
-
-			<ul class="cd-nav-gallery is-hidden">
-				<li class="go-back"><a href="#0">Menu</a></li>
-				<li class="see-all"><a href="#">NEWS&gt;</a></li>
-
-<?php
-
-	echo berea_get_news_for_universal_nav();
-
-?>
-
-
-			</ul>
-		</li>
-
-		<li class="">
-			<a href="http://community.berea.edu/calendar/index.php?calendar=default">EVENTS</a>
-		</li>
-
-
-		<li>
-			<a href="http://dev.berea.edu/subsite/">GIVE</a>
-		</li>
+		<?php include('universal_menu_master.php'); ?>
+	<?php } else { ?>
+		<!-- just master menu - no subsite menu -->
+		<?php include('universal_menu_master.php'); ?>
+	<?php } ?>
 
 	</ul> <!-- primary-nav -->
 </nav> <!-- cd-nav -->
