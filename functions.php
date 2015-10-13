c7eb4099779ec78531de5624139bbd19eea38993
Ron Northrip <ron.northrip@gmail.com>
2015-10-13 11:07:37 -0400

    Removing berea_script_styles and consolidating into berea_scripts in functions.php

--- functions.php
+++ functions.php
@@ -115,43 +115,14 @@ add_action( 'after_setup_theme', 'berea_setup' );
  */
 if ( !function_exists('berea_scripts') ) :
 
-	
-    function berea_scripts_styles() {
-    
-        /*   REGISTER MEGA DROPDOWN SCRIPTS */
-        //removed the mega-site-navigation from the filepath
-        wp_register_script('megadropdown_modernizr', get_template_directory_uri().'/assets/js/vendor/modernizr.js');
-
-        /*   Change from WP default jQuery version to 2.2.1 (used by mega-site-navigation) */
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', get_template_directory_uri().'/assets/js/vendor/jquery-2.1.1.js');  
-
-
-        //wp_register_script('megadropdown_jquery_menu_aim', get_template_directory_uri().'/assets/mega-site-navigation/js/jquery.menu-aim.js');
-        //wp_register_script('megadropdown_main', get_template_directory_uri().'/assets/js/vendor/navigation.js');
-    
-        /*   REGISTER MEGA DROPDOWN STYLES <-- integrated with the other css files 	*/
-    
-    
-        /*   CALL ALL CSS AND SCRIPTS FOR SITE */
-        wp_enqueue_script('megadropdown_modernizr', array(), 'false');
-        wp_enqueue_script('jquery');  
-        wp_enqueue_script('megadropdown_jquery_menu_aim');
-        wp_enqueue_script('megadropdown_main');
-    
-    	//not sure if needed since it's now consolidated.
-        wp_enqueue_style('megadropdown_reset');
-        wp_enqueue_style('megadropdown_style');
-    }
-    add_action( 'wp_enqueue_scripts', 'berea_scripts_styles' );	
-
-
-
 	function berea_scripts() {
 
 		/* temporaryily enforcing unminified version
 		#TODO: allow script debug after minify workflow is resolved */
 		if ( TRUE || SCRIPT_DEBUG || WP_DEBUG ) :
+			// jQuery 2.1.1 - required for megadropdown scripts
+        	wp_deregister_script('jquery');
+        	wp_register_script('jquery', get_template_directory_uri().'/assets/js/jquery-2.1.1.js');  
 
 			// Concatonated Scripts
 			wp_enqueue_script( 'production-js', get_template_directory_uri() . '/assets/js/production.js', array( 'jquery' ), '1.0.0', false );
@@ -165,7 +136,11 @@ if ( !function_exists('berea_scripts') ) :
 			}
 
 		else :
-			// Concatonated Scripts
+			// jQuery 2.1.1 - required for megadropdown scripts
+        	wp_deregister_script('jquery');
+        	wp_register_script('jquery', get_template_directory_uri().'/assets/js/jquery-2.1.1.min.js');
+        	
+			// Concatenated Scripts
 			wp_enqueue_script( 'production-js', get_template_directory_uri() . '/assets/js/production-min.js', array( 'jquery' ), '1.0.0', false );
 
 			// Main Style
