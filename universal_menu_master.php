0a27e26d922a119ab2397577ed288445efcb6435
Charlie Campbell <hellocharlie@gmail.com>
2015-12-03 15:07:06 -0500

    Incremental Change to Navigation
    In progress.

--- universal_menu_master.php
+++ universal_menu_master.php
@@ -4,67 +4,67 @@
 
 			<ul class="cd-secondary-nav is-hidden">
 				<li class="go-back"><a href="#0">BACK</a></li>
-				<li class="see-all"><a href="http://www.berea.edu/admissions/">ADMISSIONS</a></li>
+				<li class="see-all"><a href="/admissions/">ADMISSIONS</a></li>
 				<li class="has-children">
-					<a href="https://www.berea.edu/admissions/">ADMISSION INFO</a>
+					<a href="/admissions/">ADMISSION INFO</a>
 
 					<ul class="is-hidden">
 						<li class="go-back"><a href="#0">BACK</a></li>
-						<li><a href="https://www.berea.edu/admissions/visit-berea-college/bereas-academic-requirements/">Admission Requirements</a></li>
-						<li><a href="https://www.berea.edu/admissions/applying-for-admission/">Application Forms</a></li>
-						<li><a href="https://www.berea.edu/admissions/applying-freshman/">New Freshmen</a></li>
-						<li><a href="https://www.berea.edu/admissions/international/">International Students</a></li>
-						<li><a href="https://www.berea.edu/admissions/transfer-application/">Transfer Students</a></li>
+						<li><a href="/admissions/visit-berea-college/bereas-academic-requirements/">Admission Requirements</a></li>
+						<li><a href="/admissions/applying-for-admission/">Application Forms</a></li>
+						<li><a href="/admissions/applying-freshman/">New Freshmen</a></li>
+						<li><a href="/admissions/international/">International Students</a></li>
+						<li><a href="/admissions/transfer-application/">Transfer Students</a></li>
 					</ul>
 				</li>
 				<li class="has-children">
-					<a href="https://www.berea.edu/campus-life/">STUDENT LIFE</a>
+					<a href="/campus-life/">STUDENT LIFE</a>
 
 					<ul class="is-hidden">
 						<li class="go-back"><a href="#0">BACK</a></li>
-						<li><a href="https://www.berea.edu/admissions/why-berea/career-preparation/">Career Preparation</a></li>
+						<li><a href="/admissions/why-berea/career-preparation/">Career Preparation</a></li>
 						<li><a href="http://www.bereaathletics.com/">Athletics</a></li>
-						<li><a href="https://www.berea.edu/cie/education-abroad-and-exchanges-menu/">Education Abroad</a></li>
-						<li><a href="https://www.berea.edu/admissions/why-berea/service-and-leadership/">Service and Leadership</a></li>
+						<li><a href="/cie/education-abroad-and-exchanges-menu/">Education Abroad</a></li>
+						<li><a href="/admissions/why-berea/service-and-leadership/">Service and Leadership</a></li>
 					</ul>
 				</li>
 				<li class="has-children">
-					<a href="https://www.berea.edu/student-financial-aid/">FINANCIAL AID</a>
+					<a href="/student-financial-aid/">FINANCIAL AID</a>
 
 					<ul class="is-hidden">
 						<li class="go-back"><a href="#0">BACK</a></li>
-						<li><a href="https://www.berea.edu/admissions/financial-aid-scholarships/bereas-tuition-scholarship/">Tuition Promise Scholarship</a></li>
-						<li><a href="https://www.berea.edu/student-financial-aid/types-of-financial-aid/">Labor Grant Aid</a></li>
-						<li><a href="https://www.berea.edu/admissions/quick-estimator/">Financial Eligibility</a></li>
+						<li><a href="/admissions/financial-aid-scholarships/bereas-tuition-scholarship/">Tuition Promise Scholarship</a></li>
+						<li><a href="/student-financial-aid/types-of-financial-aid/">Labor Grant Aid</a></li>
+						<li><a href="/admissions/quick-estimator/">Financial Eligibility</a></li>
 						<li><a href="https://berea.studentaidcalculator.com">Estimate First-Year Costs</a></li>
-						<li><a href="https://www.berea.edu/admissions/applying-for-admission/">Submit and RFQ</a></li>
+						<li><a href="/admissions/applying-for-admission/">Submit and RFQ</a></li>
 					</ul>
 				</li>
 				<li class="has-children menu-item-show-on-large">
-					<div class="cd-item-as-button button-green"><a id="admissions-apply-button" href="https://www.berea.edu/admissions/applying-for-admission/">APPLY TODAY</a></div>
-					<div class="cd-item-as-button button-blue"><a id="admissions-eligible-button" href="https://www.berea.edu/admissions/quick-estimator/">CONFIRM YOUR FINANCIAL ELIGIBILITY</a></div>
-					<div class="cd-item-as-button button-purple"><a id="admissions-visit-button" href="https://www.berea.edu/admissions/visit-campus/">SCHEDULE A CAMPUS VISIT</a></div>
+					<div class="cd-item-as-button button-green"><a id="admissions-apply-button" href="/admissions/applying-for-admission/">APPLY TODAY</a></div>
+					<div class="cd-item-as-button button-blue"><a id="admissions-eligible-button" href="/admissions/quick-estimator/">CONFIRM YOUR FINANCIAL ELIGIBILITY</a></div>
+					<div class="cd-item-as-button button-purple"><a id="admissions-visit-button" href="/admissions/visit-campus/">SCHEDULE A CAMPUS VISIT</a></div>
 				</li>
-				<li class="cd-item-as-button menu-item-hide-on-large button-green"><a id="admissions-apply-button" href="https://www.berea.edu/admissions/applying-for-admission/">APPLY TODAY</a></li>
-				<li class="cd-item-as-button menu-item-hide-on-large button-blue"><a id="admissions-eligible-button"href="https://www.berea.edu/admissions/quick-estimator/">CONFIRM YOUR FINANCIAL ELIGIBILITY</a></li>
-				<li class="cd-item-as-button menu-item-hide-on-large button-purple"><a id="admissions-visit-button" href="https://www.berea.edu/admissions/visit-campus/">SCHEDULE A CAMPUS VISIT</a></li>
+				<li class="cd-item-as-button menu-item-hide-on-large button-green"><a id="admissions-apply-button" href="/admissions/applying-for-admission/">APPLY TODAY</a></li>
+				<li class="cd-item-as-button menu-item-hide-on-large button-blue"><a id="admissions-eligible-button"href="/admissions/quick-estimator/">CONFIRM YOUR FINANCIAL ELIGIBILITY</a></li>
+				<li class="cd-item-as-button menu-item-hide-on-large button-purple"><a id="admissions-visit-button" href="/admissions/visit-campus/">SCHEDULE A CAMPUS VISIT</a></li>
 			</ul>
 		</li>
 
 		<li class="has-children">
-			<a href="https://www.berea.edu/academics/">ACADEMICS</a>
+			<a href="/academics/">ACADEMICS</a>
 			<ul class="cd-nav-icons is-hidden">
 				<li class="go-back"><a href="#0">BACK</a></li>
-				<li class="see-all"><a href="https://www.berea.edu/academics/">ACADEMICS</a></li>
+				<li class="see-all"><a href="/academics/">ACADEMICS</a></li>
 				<li>
-					<a class="cd-nav-item item-1" href="https://www.berea.edu/academics/">
+					<a class="cd-nav-item item-1" href="/academics/">
 						<h3>DEGREE PROGRAMS</h3>
 						<p>Bachelor of Arts and Bachelor of Science degrees in 28 fields.</p>
 					</a>
 				</li>
 
 				<li>
-					<a class="cd-nav-item item-2" href="https://www.berea.edu/labor-program/">
+					<a class="cd-nav-item item-2" href="/labor-program/">
 						<h3>LABOR PROGRAM</h3>
 						<p>Students are engaged in a learning environment that encourages them to be active learners, workers, and servers.</p>
 					</a>
@@ -78,7 +78,7 @@
 				</li>
 
 				<li>
-					<a class="cd-nav-item item-4" href="https://www.berea.edu/academics/">
+					<a class="cd-nav-item item-4" href="/academics/">
 						<h3>MAJORS &amp; MINORS</h3>
 						<p>Bachelor of Arts and Bachelor of Science degrees in 28 fields.</p>
 					</a>
@@ -92,7 +92,7 @@
 				</li>
 
 				<li>
-					<a class="cd-nav-item item-6" href="https://www.berea.edu/academics/">
+					<a class="cd-nav-item item-6" href="/academics/">
 						<h3>CENTERS</h3>
 						<p>Berea College boasts seven centers which serve our campus and community in a variety of ways.</p>
 					</a>
@@ -106,14 +106,14 @@
 				</li>
 
 				<li>
-					<a class="cd-nav-item item-8" href="https://www.berea.edu/campus/">
+					<a class="cd-nav-item item-8" href="/campus/">
 						<h3>RESOURCES</h3>
 						<p>Explore common resources for campus and community.</p>
 					</a>
 				</li>
 
 				<li>
-					<a class="cd-nav-item item-8" href="https://www.berea.edu/avp/faculty-scholarly-works-2013-2014/">
+					<a class="cd-nav-item item-8" href="/avp/faculty-scholarly-works-2013-2014/">
 						<h3>FACULTY &amp; STAFF SCHOLARLY WORK</h3>
 						<p>While our faculty proudly focuses on being and becoming excellent classroom teachers, they are also active intellectuals and scholars.</p>
 					</a>
@@ -148,26 +148,26 @@
 
 
 		<li>
-			<a href="https://www.berea.edu/give-to-berea/the-berea-fund/">GIVE</a>
+			<a href="/give-to-berea/the-berea-fund/">GIVE</a>
 		</li>
 
 		<li class="has-children">
-			<a href="#">ABOUT</a>
+			<a href="/about">ABOUT</a>
 
 			<ul class="cd-secondary-nav is-hidden">
 				<li class="go-back"><a href="#0">BACK</a></li>
-				<li class="see-all"><a href="#">ABOUT</a></li>
+				<li class="see-all"><a href="/about">ABOUT</a></li>
 				<li class="has-children">
 					<a href="#">OVERVIEW</a>
 
 					<ul class="is-hidden">
 						<li class="go-back"><a href="#0">BACK</a></li>
 
-						<li><a href="#">The Berea College Difference</a></li>
+						<!-- <li><a href="#">The Berea College Difference</a></li> -->
 						<li><a href="#">Quick Facts/Berea at a Glance</a></li>
-						<li><a href="#">Ratings and Rankings</a></li>
-						<li><a href="#">Berea in Words</a></li>
-						<li><a href="#">Berea in Photographs</a></li>
+						<!-- <li><a href="#">Ratings and Rankings</a></li> -->
+						<!-- <li><a href="#">Berea in Words</a></li> -->
+						<!-- <li><a href="#">Berea in Photographs</a></li> -->
 						<li><a href="#">Our Mission</a></li>
 						<li><a href="#">Our Vision</a></li>
 						<li><a href="#">The Great Commitments</a></li>
