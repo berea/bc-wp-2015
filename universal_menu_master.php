d61a76213a1ccf5c5d30cc514d96e0cc5877341e
Charlie Campbell <hellocharlie@gmail.com>
2015-12-07 08:21:40 -0500

    Moved Location of About in Main Navigation

--- universal_menu_master.php
+++ universal_menu_master.php
@@ -1,3 +1,95 @@
+<li class="has-children">
+	<a href="/about">ABOUT</a>
+
+	<ul class="cd-secondary-nav is-hidden">
+		<li class="go-back"><a href="#0">BACK</a></li>
+		<li class="see-all"><a href="/about">ABOUT</a></li>
+		<li class="has-children">
+			<a href="#">OVERVIEW</a>
+
+			<ul class="is-hidden">
+				<li class="go-back"><a href="#0">BACK</a></li>
+
+				<!-- <li><a href="#">The Berea College Difference</a></li> -->
+				<li><a href="#">Quick Facts/Berea at a Glance</a></li>
+				<!-- <li><a href="#">Ratings and Rankings</a></li> -->
+				<!-- <li><a href="#">Berea in Words</a></li> -->
+				<!-- <li><a href="#">Berea in Photographs</a></li> -->
+				<li><a href="#">Our Mission</a></li>
+				<li><a href="#">Our Vision</a></li>
+				<li><a href="#">The Great Commitments</a></li>
+				<li><a href="#">History &amp; Heritage</a></li>
+			</ul>
+		</li>
+
+		<li class="has-children">
+			<a href="#">LEADERSHIP AND GOVERNANCE</a>
+
+			<ul class="is-hidden">
+				<li class="go-back"><a href="#0">BACK</a></li>
+
+				<li><a href="#">Office of the President/President's Message</a></li>
+				<li><a href="#">Administrative Committee</a></li>
+				<li><a href="#">Board of Trustees</a></li>
+			</ul>
+		</li>
+
+		<li class="has-children">
+				<a href="#">ADMINISTRATIVE OFFICES</a>
+
+				<ul class="is-hidden">
+					<li class="go-back"><a href="#0">BACK</a></li>
+					<li class="see-all"><a href="#">ADMINISTRATIVE OFFICES</a></li>
+					<li><a href="#">Admissions</a></li>
+					<li><a href="#">Alumni Relations</a></li>
+					<li><a href="#">Development/Giving to Berea</a></li>
+					<li><a href="#">Human Resources</a></li>
+					<li><a href="#">Integrated Marketing &amp; Communications</a></li>
+					<li><a href="#">Information Systems and Services</a></li>
+					<li><a href="#">Public Safety</a></li>
+					<li><a href="#">Hutchins Library</a></li>
+					<li class="has-children">
+						<a href="#0">CENTERS</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">CENTERS</a></li>
+							<li class="see-all"><a href="#">ALL CENTERS</a></li>
+							<li><a href="#">Partners for Education</a></li>
+							<li><a href="#">Loyal Jones Appalachian Center</a></li>
+							<li><a href="#">bell hooks Institute</a></li>
+							<li><a href="#">Brushy Fork Institute</a></li>
+							<li><a href="#">Carter G. Woodson Center for Interracial Education</a></li>
+							<li><a href="#">Black Cultural Center</a></li>
+							<li><a href="#">Center for Excellence in Learning Through Service (CELTS)</a></li>
+							<li><a href="#">Center for Transformative Learning</a></li>
+							<li><a href="#">Francis and Louise Hutchins Center for International Education</a></li>
+							<li><a href="#">Willis D. Weatherford, Jr. Campus Christian Center</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+
+		<li class="has-children">
+			<a href="#">TITLE TBD</a>
+
+			<ul class="is-hidden">
+				<li class="go-back"><a href="#0">BACK</a></li>
+				<li><a href="#">Newsroom</a></li>
+				<li><a href="#">Visit Berea College/Visitors Center</a></li>
+				<li><a href="#">Virtual Tour</a></li>
+				<li><a href="#">Bookstore</a></li>
+				<li><a href="#">Contact Berea</a></li>
+				<li><a href="#">Historic Boone Tavern</a></li>
+				<li><a href="#">Student Crafts</a></li>
+				<li><a href="#">Conference Center</a></li>
+				<li><a href="#">Seabury Athletic Center</a></li>
+				<li><a href="#">Employment Opportunities</a></li>
+			</ul>
+		</li>
+
+	</ul>
+</li>
+
 
 		<li class="has-children">
 			<a href="#">ADMISSIONS</a>
@@ -149,96 +241,4 @@
 
 		<li>
 			<a href="/give-to-berea/the-berea-fund/">GIVE</a>
-		</li>
-
-		<li class="has-children">
-			<a href="/about">ABOUT</a>
-
-			<ul class="cd-secondary-nav is-hidden">
-				<li class="go-back"><a href="#0">BACK</a></li>
-				<li class="see-all"><a href="/about">ABOUT</a></li>
-				<li class="has-children">
-					<a href="#">OVERVIEW</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">BACK</a></li>
-
-						<!-- <li><a href="#">The Berea College Difference</a></li> -->
-						<li><a href="#">Quick Facts/Berea at a Glance</a></li>
-						<!-- <li><a href="#">Ratings and Rankings</a></li> -->
-						<!-- <li><a href="#">Berea in Words</a></li> -->
-						<!-- <li><a href="#">Berea in Photographs</a></li> -->
-						<li><a href="#">Our Mission</a></li>
-						<li><a href="#">Our Vision</a></li>
-						<li><a href="#">The Great Commitments</a></li>
-						<li><a href="#">History &amp; Heritage</a></li>
-					</ul>
-				</li>
-
-				<li class="has-children">
-					<a href="#">LEADERSHIP AND GOVERNANCE</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">BACK</a></li>
-
-						<li><a href="#">Office of the President/President's Message</a></li>
-						<li><a href="#">Administrative Committee</a></li>
-						<li><a href="#">Board of Trustees</a></li>
-					</ul>
-				</li>
-
-				<li class="has-children">
-						<a href="#">ADMINISTRATIVE OFFICES</a>
-
-						<ul class="is-hidden">
-							<li class="go-back"><a href="#0">BACK</a></li>
-							<li class="see-all"><a href="#">ADMINISTRATIVE OFFICES</a></li>
-							<li><a href="#">Admissions</a></li>
-							<li><a href="#">Alumni Relations</a></li>
-							<li><a href="#">Development/Giving to Berea</a></li>
-							<li><a href="#">Human Resources</a></li>
-							<li><a href="#">Integrated Marketing &amp; Communications</a></li>
-							<li><a href="#">Information Systems and Services</a></li>
-							<li><a href="#">Public Safety</a></li>
-							<li><a href="#">Hutchins Library</a></li>
-							<li class="has-children">
-								<a href="#0">CENTERS</a>
-
-								<ul class="is-hidden">
-									<li class="go-back"><a href="#0">CENTERS</a></li>
-									<li class="see-all"><a href="#">ALL CENTERS</a></li>
-									<li><a href="#">Partners for Education</a></li>
-									<li><a href="#">Loyal Jones Appalachian Center</a></li>
-									<li><a href="#">bell hooks Institute</a></li>
-									<li><a href="#">Brushy Fork Institute</a></li>
-									<li><a href="#">Carter G. Woodson Center for Interracial Education</a></li>
-									<li><a href="#">Black Cultural Center</a></li>
-									<li><a href="#">Center for Excellence in Learning Through Service (CELTS)</a></li>
-									<li><a href="#">Center for Transformative Learning</a></li>
-									<li><a href="#">Francis and Louise Hutchins Center for International Education</a></li>
-									<li><a href="#">Willis D. Weatherford, Jr. Campus Christian Center</a></li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-
-				<li class="has-children">
-					<a href="#">TITLE TBD</a>
-
-					<ul class="is-hidden">
-						<li class="go-back"><a href="#0">BACK</a></li>
-						<li><a href="#">Newsroom</a></li>
-						<li><a href="#">Visit Berea College/Visitors Center</a></li>
-						<li><a href="#">Virtual Tour</a></li>
-						<li><a href="#">Bookstore</a></li>
-						<li><a href="#">Contact Berea</a></li>
-						<li><a href="#">Historic Boone Tavern</a></li>
-						<li><a href="#">Student Crafts</a></li>
-						<li><a href="#">Conference Center</a></li>
-						<li><a href="#">Seabury Athletic Center</a></li>
-						<li><a href="#">Employment Opportunities</a></li>
-					</ul>
-				</li>
-
-			</ul>
 		</li>
