96c9ee45d1a45dded767e1c33e1a5d4dff929a15
Scott Horn <jscotthorn@gmail.com>
2016-01-15 14:08:28 -0500

    legacy wall

--- assets/js/legacy-wall.js
+++ assets/js/legacy-wall.js
@@ -5,5 +5,40 @@ jQuery(document).ready(function( $ ) {
 
 	$.getJSON('/wp-content/themes/bc-wp-2015/assets/js/custom/legacy-wall.json', function(data) {
 		console.log(data);
+		data.panelNames.sort();
+
+		// Generate year and names lists
+		var years = {}
+		var yearPanels = [];
+		var names = {};
+		var nameMap = [];
+		$.each(data.panelNames, function(i, panel) {
+			var year panel.split(' ').unshift();
+			if (!inArray(year, years)) {
+				years.push(year);
+			}
+			if (!year in yearPanels) {
+				yearPanels[year] = [];
+			}
+			yearPanels[year].push(panel);
+			$.each(panels[panel].names, function(j, name){
+				names.push(name);
+				nameMap[name] = panel;
+			});
+		});
+		console.log(years);
+		console.log(yearPanels);
+		console.log(nameMap);
+
 	});
 });
+
+
+
+function inArray(needle, haystack) {
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+        if(haystack[i] == needle) return true;
+    }
+    return false;
+}
