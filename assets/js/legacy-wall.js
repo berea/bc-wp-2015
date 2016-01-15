42255169624ddafe91783096a70c144417d4d754
Scott Horn <jscotthorn@gmail.com>
2016-01-15 13:55:56 -0500

    legacy wall

--- assets/js/legacy-wall.js
+++ assets/js/legacy-wall.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function( $ ) {
 	console.log('legacy wall');
 	$('.post .entry-content').append($('<div class="loading"></div>'));
 
-	$.getJSON('/wp-content/bc-wp-2015/assets/js/custom/legacy-wall.json', function(data) {
+	$.getJSON('/wp-content/themes/bc-wp-2015/assets/js/custom/legacy-wall.json', function(data) {
 		console.log(data);
 	});
 });
