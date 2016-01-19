f5c1ef0c40d49e8ebe0051edaf07bf6192552ee5
Scott Horn <jscotthorn@gmail.com>
2016-01-19 16:20:03 -0500

    Link lw names to their year.

--- page-templates/partials/content-legacy-wall-name.php
+++ page-templates/partials/content-legacy-wall-name.php
@@ -3,14 +3,13 @@
  * @package berea
  */
 $terms = wp_get_post_terms(get_the_ID(), 'legacy-wall-year');
-d($terms);
 ?>
 
 <?php tha_entry_before(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php tha_entry_top(); ?>
 	<header class="entry-header">
-		<div class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></div>
+		<div class="entry-title"><a href="<?php print get_term_link($terms[0]); ?>" rel="bookmark"><?php the_title(); ?></a></div>
 	</header><!-- .entry-header -->
 	<?php tha_entry_bottom(); ?>
 </article><!-- #post-## -->
