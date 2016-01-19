cc8154bb591e10e2eab61e8f2b2cdda4ca219281
Scott Horn <jscotthorn@gmail.com>
2016-01-19 15:02:31 -0500

    Removed default title and added ordering.

--- archive-legacy-wall-names.php
+++ archive-legacy-wall-names.php
@@ -14,7 +14,8 @@ $results = $wpdb->get_results(
     SELECT * FROM $wpdb->posts
     WHERE post_title LIKE '$letter%'
     AND post_type = 'legacy-wall-names'
-    AND post_status = 'publish'; 
+    AND post_status = 'publish'
+    ORDER BY title ASC; 
     "
 ); 
 
@@ -28,71 +29,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h2>Legacy Wall - <?php print $letter; ?></h2>
-				<h1 class="page-title">
-					<?php
-					if (function_exists('get_the_archive_title')) :
-						echo get_the_archive_title();
-
-						/*
-						* TO-DO Might remove this code block at some point, since
-						*	get_the_archive_title() does the same thing
-						*	the below code does
-						*/
-						elseif ( is_category() ):
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							/* Queue the first post, that way we know
-							 * what author we're dealing with (if that is the case).
-							*/
-							the_post();
-							printf( __( 'Author: %s', 'berea' ), '<span class="vcard">' . get_the_author() . '</span>' );
-							/* Since we called the_post() above, we need to
-							 * rewind the loop back to the beginning that way
-							 * we can run the loop properly, in full.
-							 */
-							rewind_posts();
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'berea' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'berea' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'berea' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'berea' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'berea' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'berea' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'berea');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'berea' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'berea' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'berea' );
-
-						elseif ( is_tax() ) :
-+							single_term_title();
-
-						else :
-							_e( 'Archives', 'berea' );
-
-						endif;
-						/*
-						* END TO-DO
-						*/
-					?>
-				</h1>
+				<h1 class="page-title">Legacy Wall - <?php print $letter; ?></h1>
 				<?php
 					// Show an optional term description.
 					$term_description = term_description();
