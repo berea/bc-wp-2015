6e0142e752e768f952e688ea9351fbae413f5aa3
Scott Horn <jscotthorn@gmail.com>
2016-01-20 10:05:05 -0500

    Add year links to lw sidebar

--- sidebar-legacy-wall.php
+++ sidebar-legacy-wall.php
@@ -12,37 +12,45 @@
 		<ul class="menu legacy-wall-sidebar">
 			<li>
 				<ul class="sub-menu alpha-links">
-					<li><a href="/legacy-wall/names/?letter=A"></a></li>
-					<li><a href="/legacy-wall/names/?letter=B"></a></li>
-					<li><a href="/legacy-wall/names/?letter=C"></a></li>
-					<li><a href="/legacy-wall/names/?letter=D"></a></li>
-					<li><a href="/legacy-wall/names/?letter=E"></a></li>
-					<li><a href="/legacy-wall/names/?letter=F"></a></li>
-					<li><a href="/legacy-wall/names/?letter=G"></a></li>
-					<li><a href="/legacy-wall/names/?letter=H"></a></li>
-					<li><a href="/legacy-wall/names/?letter=I"></a></li>
-					<li><a href="/legacy-wall/names/?letter=J"></a></li>
-					<li><a href="/legacy-wall/names/?letter=K"></a></li>
-					<li><a href="/legacy-wall/names/?letter=L"></a></li>
-					<li><a href="/legacy-wall/names/?letter=M"></a></li>
-					<li><a href="/legacy-wall/names/?letter=N"></a></li>
-					<li><a href="/legacy-wall/names/?letter=O"></a></li>
-					<li><a href="/legacy-wall/names/?letter=P"></a></li>
-					<li><a href="/legacy-wall/names/?letter=Q"></a></li>
-					<li><a href="/legacy-wall/names/?letter=R"></a></li>
-					<li><a href="/legacy-wall/names/?letter=S"></a></li>
-					<li><a href="/legacy-wall/names/?letter=T"></a></li>
-					<li><a href="/legacy-wall/names/?letter=U"></a></li>
-					<li><a href="/legacy-wall/names/?letter=V"></a></li>
-					<li><a href="/legacy-wall/names/?letter=W"></a></li>
-					<li><a href="/legacy-wall/names/?letter=X"></a></li>
-					<li><a href="/legacy-wall/names/?letter=Y"></a></li>
-					<li><a href="/legacy-wall/names/?letter=Z"></a></li>
+					<li><a href="/legacy-wall/names/?letter=A">A</a></li>
+					<li><a href="/legacy-wall/names/?letter=B">B</a></li>
+					<li><a href="/legacy-wall/names/?letter=C">C</a></li>
+					<li><a href="/legacy-wall/names/?letter=D">D</a></li>
+					<li><a href="/legacy-wall/names/?letter=E">E</a></li>
+					<li><a href="/legacy-wall/names/?letter=F">F</a></li>
+					<li><a href="/legacy-wall/names/?letter=G">G</a></li>
+					<li><a href="/legacy-wall/names/?letter=H">H</a></li>
+					<li><a href="/legacy-wall/names/?letter=I">I</a></li>
+					<li><a href="/legacy-wall/names/?letter=J">J</a></li>
+					<li><a href="/legacy-wall/names/?letter=K">K</a></li>
+					<li><a href="/legacy-wall/names/?letter=L">L</a></li>
+					<li><a href="/legacy-wall/names/?letter=M">M</a></li>
+					<li><a href="/legacy-wall/names/?letter=N">N</a></li>
+					<li><a href="/legacy-wall/names/?letter=O">O</a></li>
+					<li><a href="/legacy-wall/names/?letter=P">P</a></li>
+					<li><a href="/legacy-wall/names/?letter=Q">Q</a></li>
+					<li><a href="/legacy-wall/names/?letter=R">R</a></li>
+					<li><a href="/legacy-wall/names/?letter=S">S</a></li>
+					<li><a href="/legacy-wall/names/?letter=T">T</a></li>
+					<li><a href="/legacy-wall/names/?letter=U">U</a></li>
+					<li><a href="/legacy-wall/names/?letter=V">V</a></li>
+					<li><a href="/legacy-wall/names/?letter=W">W</a></li>
+					<li><a href="/legacy-wall/names/?letter=X">X</a></li>
+					<li><a href="/legacy-wall/names/?letter=Y">Y</a></li>
+					<li><a href="/legacy-wall/names/?letter=Z">Z</a></li>
 				</ul>
 			</li>
 			<li>
 				<ul class="sub-menu">
-					
+				<?php
+					 $terms = get_terms( 'my_taxonomy' );
+					 if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+					     foreach ( $terms as $term ) {
+					       echo '<li><a href="' . get_term_link($term) . '">' . $term->name . '</a></li>';
+					        
+					     }
+					 }
+				?>
 				</ul>
 			</li>
 		</ul>
