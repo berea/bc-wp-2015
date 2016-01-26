ce47099bd5b341f9b0899e4d0ee5f5196e4904b8
Scott Horn <jscotthorn@gmail.com>
2016-01-26 10:39:42 -0500

    Normalize news sidebar html

--- sidebar-news.php
+++ sidebar-news.php
@@ -10,15 +10,16 @@
 		<div id="secondary-inner">
 		<?php tha_sidebar_top(); ?>
 		<?php do_action( 'before_sidebar' ); ?>
-
-		<h4>News Archive</h4>
-		<?php wp_get_archives(['type'=>'yearly']); ?>
-
-		<h4>Stay in Touch</h4>
-		<ul class="submenu">
-			<li><a href="/newsroom">Berea Newsroom</a></li>
+		<ul class="menu">
+			<li><h4>News Archive</h4></li>
+			<li><ul class="sub-menu">
+				<?php wp_get_archives(['type'=>'yearly']); ?>
+			</ul></li>
+			<li><h4>Stay in Touch</h4></li>
+			<li><ul class="submenu">
+				<li><a href="/newsroom">Berea Newsroom</a></li>
+			</ul></li>
 		</ul>
-
 		<?php tha_sidebar_bottom(); ?>
 		</div>
 	</div><!-- #secondary -->
