865e981ad93cf5c7e949ff2b8a6389c8908bc294
Scott Horn <jscotthorn@gmail.com>
2016-01-26 10:52:07 -0500

    More news sidebar normalizing, changing news posts to links.

--- page-newsroom.php
+++ page-newsroom.php
@@ -27,7 +27,7 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'page-templates/partials/content', 'page' );
+					get_template_part( 'page-templates/partials/content', 'link' );
 				?>
 
 			<?php endwhile; ?>
--- sidebar-news.php
+++ sidebar-news.php
@@ -11,13 +11,13 @@
 		<?php tha_sidebar_top(); ?>
 		<?php do_action( 'before_sidebar' ); ?>
 		<ul class="menu">
-			<li><a href="/news">News Archive</a></li>
+			<li class="menu-item"><a href="/news">News Archive</a></li>
 			<li><ul class="sub-menu">
 				<?php wp_get_archives(['type'=>'yearly']); ?>
 			</ul></li>
-			<li><a href="/newsroom">Stay in Touch</a></li>
-			<li><ul class="submenu">
-				<li><a href="/newsroom">Berea Newsroom</a></li>
+			<li class="menu-item"><a href="/newsroom">Stay in Touch</a></li>
+			<li><ul class="sub-menu">
+				<li class="menu-item"><a href="/newsroom">Berea Newsroom</a></li>
 			</ul></li>
 		</ul>
 		<?php tha_sidebar_bottom(); ?>
