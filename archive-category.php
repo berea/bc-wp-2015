f8013b319ff6313767ab2e905554738444a5e7d9
Scott Horn <jscotthorn@gmail.com>
2016-02-08 14:57:21 -0500

    Category archive template

--- archive-category.php
+++ archive-category.php
@@ -7,6 +7,9 @@
  * @package berea
  */
 
+$cat = get_query_var('catarchive');
+$cat = get_term($cat);
+
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
@@ -17,68 +20,7 @@ get_header(); ?>
 
 			<header class="page-header">
 				<h1 class="page-title">
-					<?php
-					if (function_exists('get_the_archive_title')) :
-						echo get_the_archive_title();
-
-						/*
-						* TO-DO Might remove this code block at some point, since
-						*	get_the_archive_title() does the same thing
-						*	the below code does
-						*/
-						elseif ( is_category() ):
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							/* Queue the first post, that way we know
-							 * what author we're dealing with (if that is the case).
-							*/
-							the_post();
-							printf( __( 'Author: %s', 'berea' ), '<span class="vcard">' . get_the_author() . '</span>' );
-							/* Since we called the_post() above, we need to
-							 * rewind the loop back to the beginning that way
-							 * we can run the loop properly, in full.
-							 */
-							rewind_posts();
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'berea' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'berea' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'berea' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'berea' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'berea' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'berea' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'berea');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'berea' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'berea' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'berea' );
-
-						elseif ( is_tax() ) :
-+							single_term_title();
-
-						else :
-							_e( 'Archives', 'berea' );
-
-						endif;
-						/*
-						* END TO-DO
-						*/
-					?>
+					<?php echo $cat->name ?> Archives
 				</h1>
 				<?php
 					// Show an optional term description.
@@ -97,7 +39,7 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'page-templates/partials/content', get_post_format() );
+					get_template_part( 'page-templates/partials/content', 'news-link' );
 				?>
 
 			<?php endwhile; ?>
@@ -112,7 +54,7 @@ get_header(); ?>
 
 	</div><!-- #primary -->
 
-	<?php get_sidebar(); ?>
+	<?php get_sidebar('news'); ?>
 
 	<div style="clear:both;"></div>
 
