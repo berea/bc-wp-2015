59c62cff89a6cf74892e3c916b5e160b1bc574ad
Scott Horn <jscotthorn@gmail.com>
2016-05-23 17:08:29 -0400

    Search style and script

--- assets/js/custom/search.js
+++ assets/js/custom/search.js
@@ -10,31 +10,89 @@
 			$('#search-page-box input[type="text"]').val(q);
 
 			// Get the search results
-			var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyA6rfuiihKZeDvjfZyWjP6qd223SYPvFZY&cx=018174935032430318208:8kij2j8mmsy&count=10&q=' + q;
+			var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyA6rfuiihKZeDvjfZyWjP6qd223SYPvFZY&cx=018174935032430318208:8kij2j8mmsy&q=' + q;
 			$.getJSON(url, function(data){
+				var start = getParameterByName('start');
 
 				// Result metadata
-				$('#search-info').html('<p><strong>' + data.searchInformation.formattedTotalResults + '</strong> results founds in <strong>' + data.searchInformation.formattedSearchTime + '</strong> seconds.</p>');
+				generateMetadata(data, start);
 
 				// Result Items
-				var html = '';
-				console.log(data);
-				for (var i = 0; i < data.items.length; i++) {
-					html += '<div class="result-item clearfix">';
-					html += '<p class="result-title"><a href="' + data.items[i].htmlFormattedUrl + '">' + data.items[i].htmlTitle + '</a></p>';
-					html += '<p class="result-url"><a href="' + data.items[i].htmlFormattedUrl + '">' + data.items[i].htmlFormattedUrl + '</a></p>';
-					html += '<p>';
-					if ('pagemap' in data.items[i] && 'cse_thumbnail' in data.items[i].pagemap) {
-						html += '<a href="' + data.items[i].htmlFormattedUrl + '"><img class="pull-left" src="' + data.items[i].pagemap.cse_thumbnail[0].src + '" alt="' + data.items[i].htmlTitle + ' (generated thumbnail)" width="' + Math.ceil(data.items[i].pagemap.cse_thumbnail[0].width/2) + '" height="' + Math.ceil(data.items[i].pagemap.cse_thumbnail[0].height/2) + '" /></a>';
-					}
-					html+= data.items[i].htmlSnippet + '</p></div>';
-				}
-				$('#results').append(html);
+				generateLinks(data);
+
+				// Pages
+				generatePages(data, start);
+				$('#search-more-links').on('click', 'a', function(){
+					// @TODO See why there is an <a> in the current <li>... shouldn't be one
+					if ($(this).parents('li.current').length > 0) return false;
+
+					var newStart = $(this).attr('data-page') * 10 - 9;
+					$.getJSON(url + '&start=' + newStart, function(newData){
+						generateMetadata(newData, newStart);
+						generatePages(newData, newStart);
+						generateLinks(newData);
+					})
+				});
 			})
 		}
 
 
 	})
+
+	function generateMetadata(data, start) {
+		var showing = (start == null) ? 'the first 10 results' : 'results ' + (start) + '-' + (start + 9);
+		$('#search-info').html('<p>Showing <strong>' + showing + '</strong> of <strong>' + data.searchInformation.formattedTotalResults + '</strong> found.</p>');
+	}
+
+	function generateLinks(data) {
+		var html = '';
+		console.log(data);
+		for (var i = 0; i < data.items.length; i++) {
+			html += '<div class="result-item clearfix">';
+			html += '<p class="result-title"><a href="' + data.items[i].htmlFormattedUrl + '">' + data.items[i].htmlTitle + '</a></p>';
+			html += '<p class="result-url"><a href="' + data.items[i].htmlFormattedUrl + '">' + data.items[i].htmlFormattedUrl + '</a></p>';
+			html += '<p>';
+			if ('pagemap' in data.items[i] && 'cse_thumbnail' in data.items[i].pagemap) {
+				html += '<a href="' + data.items[i].htmlFormattedUrl + '"><img class="pull-left" src="' + data.items[i].pagemap.cse_thumbnail[0].src + '" alt="' + data.items[i].htmlTitle + ' (generated thumbnail)" width="' + Math.ceil(data.items[i].pagemap.cse_thumbnail[0].width/2) + '" height="' + Math.ceil(data.items[i].pagemap.cse_thumbnail[0].height/2) + '" /></a>';
+			}
+			html+= data.items[i].htmlSnippet + '</p></div>';
+		}
+		$('#results').html(html);
+	}
+
+	function generatePages(data, start) {
+		var currentPage = (start == null) ? 1 : (start - 1)/10 + 1;
+		var totalPages = Math.ceil(data.searchInformation.totalResults / 10);
+		var firstPage = (currentPage <= 6) ? 1 : currentPage - 5;
+		var lastPage = (currentPage <= 6) ? 10 : currentPage + 4;
+		if (totalPages < lastPage) lastPage = totalPages;
+		console.log("current: " + currentPage);
+		console.log("first: " + firstPage);
+		console.log("last: " + lastPage);
+		
+		var pages = '<ul>';
+		// Previous link
+		if (currentPage > 1) {
+			pages += '<li class="previous"><a data-page="' + (currentPage - 1) + '">Previous</a></li>';
+		}
+		// Page links
+		for (var i = firstPage; i <= lastPage; i++) {
+			if (i == currentPage) {
+				pages += '<li class="current">' + i + '</li>';
+			}
+			else {
+				pages += '<li><a data-page="' + i + '">' + i + '</li>';
+			}
+		}
+		// Next link
+		if (currentPage < lastPage) {
+			pages += '<li class="next"><a data-page="' + (currentPage + 1) + '">Next</a></li>';
+		}
+		// Close the list
+		pages += '</ul>';
+
+		$('#search-more-links').html(pages);
+	}
 
 	function getParameterByName(name, url) {
 	    if (!url) url = window.location.href;
