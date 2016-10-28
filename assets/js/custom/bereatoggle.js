618fc54af4387ba9d168e1b29672606d35cd4d53
Dan Adams <daniel_adams@berea.edu>
2016-10-28 17:04:58 -0400

    removing "strict" from jsintrc, moved legacy-wall.js from the custom folder (unless I'm mistaken, we don't want that rolled into production.js).  Added some basic toggle code just to get it working (took some time tooling around with Visual Composer), but we'll modify that code again shortly.

---
+++ assets/js/custom/bereatoggle.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+
+
+    //$("#commitment_toggle_liberalarts").show();
+	//$("#commitment_toggle_liberalarts").hide();
+
+	$("#commitment_liberalarts").click(function(){
+        $("#commitment_toggle_liberalarts").slideToggle("slow");
+    });
+
+
+});
+
