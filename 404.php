da886641efa72b4e2bf31e3f7fb4058b158645d5
Dan Adams <daniel_adams@berea.edu>
2017-06-08 15:26:07 -0400

    redirecting the 404 to our "page-not-found" page.

--- 404.php
+++ 404.php
@@ -1,38 +1,4 @@
 <?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package berea
- */
-
-get_header(); ?>
-
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-
-        <?php
-
-        $page = get_posts(
-            array(
-                'name'      => 'page-not-found',
-                'post_type' => 'page'
-            )
-        );
-        ?>
-
-        <div class="entry-content">
-
-            <?php echo $page[0]->post_content; ?>
-
-        </div>
-
-        <?php
-            /* Restore original Post Data */
-            wp_reset_postdata();
-        ?>
-
-    </main><!-- #main -->
-
-</div><!-- #primary -->
-
-<?php get_footer(); ?>
+    header("Status: 301 Moved Permanently");
+    header("Location:http://berea.dea.dev/page-not-found/");
+?>
