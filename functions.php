bcbc5d8d218be9485aef54074df41f1cbc7d8009
Dan Adams <daniel_adams@berea.edu>
2017-08-31 10:36:53 -0400

    adding versioning quick fix for caching issue.

--- functions.php
+++ functions.php
@@ -144,7 +144,7 @@ if ( !function_exists('berea_scripts') ) :
 
 			// Main Style
 			if (wp_get_theme()->name != 'Berea 2015 - Stopgap Child Theme') {
-				wp_enqueue_style( 'berea-style',  get_template_directory_uri() . '/assets/css/style.css' );
+				wp_enqueue_style( 'berea-style',  get_template_directory_uri() . '/assets/css/style-v1.css' );
 			}
 			else {
 				wp_enqueue_style( 'berea-style',  get_template_directory_uri() . '/assets/css/style-stopgap.css' );
