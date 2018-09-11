6b7f460911f4c424f1458206a5a4e1149a879757
Joseph Carrick <TelaJoseph_Carrick@berea.edu>
2018-09-11 13:56:02 -0400

    Added usability analytics script from Siteimprove into production.js file

---
+++ assets/js/custom/usability-analytics.js
@@ -0,0 +1,9 @@
+// Usability analytics script from Siteimprove
+
+/*<![CDATA[*/
+(function() {
+ var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+ sz.src = '//siteimproveanalytics.com/js/siteanalyze_90779361.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/
--- assets/js/production.js
+++ assets/js/production.js
@@ -2593,4 +2593,13 @@ function inArray(needle, haystack) {
     }
     return false;
 }
+// Usability analytics script from Siteimprove
+
+/*<![CDATA[*/
+(function() {
+ var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+ sz.src = '//siteimproveanalytics.com/js/siteanalyze_90779361.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/
 //# sourceMappingURL=production.js.map
