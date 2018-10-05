d57676f1145b0db7d7b966c93c0b05bb57a8f9ed
Joseph Carrick <TelaJoseph_Carrick@berea.edu>
2018-10-05 17:37:20 -0400

    Style change for "line separator with text" vc element.

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
  * Two Column, Right-Sidebar
  *
  * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css?c09c0a4bc8).
+ * and one of the two required files for a theme (the other being style.css?c677735ae8).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
