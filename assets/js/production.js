f90bc8ae6d5f441ae497ac8e297209834c01f5c4
Dan Adams <daniel_adams@berea.edu>
2019-06-20 09:08:43 -0400

    adding tracking code for Admissions (marketing partner)

--- assets/js/production.js
+++ assets/js/production.js
@@ -2323,7 +2323,6 @@ References:
 	};
 })(this);
 
-
 /*  Berea Custom JavaScripts (empty currently)  */
 
 (function($) {
@@ -2607,6 +2606,21 @@ function inArray(needle, haystack) {
     }
     return false;
 }
+// tracking code for Admissions partner - 
+
+
+   var _ss = _ss || [];
+   _ss.push(['_setDomain', 'https://koi-3QNJCQ0Q7O.marketingautomation.services/net']);
+   _ss.push(['_setAccount', 'KOI-44JNNTK8CO']);
+   _ss.push(['_trackPageView']);
+(function() {
+   var ss = document.createElement('script');
+   ss.type = 'text/javascript'; ss.async = true;
+   ss.src = ('https:' == document.location.protocol ? '[https://' : 'http://]https://' : 'http://') + 'koi-3QNJCQ0Q7O.marketingautomation.services/client/ss.js?ver=1.1.1';
+   var scr = document.getElementsByTagName('script')[0];
+   scr.parentNode.insertBefore(ss, scr);
+})();
+
 /**
  * @file The core file for the events calendar plugin javascript.
  * This file must load on all front facing events pages and be the first file loaded after vendor dependencies.
