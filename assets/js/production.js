ec42b4ea9e438440f747435fd4d7f31557a15319
Dan Adams <daniel_adams@berea.edu>
2019-06-25 11:08:30 -0400

    trying tracking code again, with an edit

--- assets/js/production.js
+++ assets/js/production.js
@@ -2607,8 +2607,22 @@ function inArray(needle, haystack) {
     return false;
 }
 // tracking code for Admissions partner - 
-/*
 
+
+   var _ss = _ss || [];
+   _ss.push(['_setDomain', 'https://koi-3QNJCQ0Q7O.marketingautomation.services/net']);
+   _ss.push(['_setAccount', 'KOI-44JNNTK8CO']);
+   _ss.push(['_trackPageView']);
+(function() {
+   var ss = document.createElement('script');
+   ss.type = 'text/javascript'; ss.async = true;
+   ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QNJCQ0Q7O.marketingautomation.services/client/ss.js?ver=1.1.1';
+   var scr = document.getElementsByTagName('script')[0];
+   scr.parentNode.insertBefore(ss, scr);
+})();
+
+
+/* OLD ONE
    var _ss = _ss || [];
    _ss.push(['_setDomain', 'https://koi-3QNJCQ0Q7O.marketingautomation.services/net']);
    _ss.push(['_setAccount', 'KOI-44JNNTK8CO']);
