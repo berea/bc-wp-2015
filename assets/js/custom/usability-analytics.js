aec013c9064314429897323339f40f3fa4d3674d
Joseph Carrick <TelaJoseph_Carrick@berea.edu>
2019-09-20 14:30:56 -0400

    Fixed a small syntax issue reported by jsHint

--- assets/js/custom/usability-analytics.js
+++ assets/js/custom/usability-analytics.js
@@ -1,9 +1,11 @@
 // Usability analytics script from Siteimprove
 
 /*<![CDATA[*/
+
 (function() {
- var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
- sz.src = '//siteimproveanalytics.com/js/siteanalyze_90779361.js';
- var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+	'use strict';
+	 var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+	 sz.src = '//siteimproveanalytics.com/js/siteanalyze_90779361.js';
+	 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
 })();
 /*]]>*/
