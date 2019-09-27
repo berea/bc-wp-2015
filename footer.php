e98bd459b76f63b09b248e694de7ccc8502f4571
Joseph Carrick <TelaJoseph_Carrick@berea.edu>
2019-09-27 11:10:12 -0400

    Fix for accessibility issue BC18-15

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@
 
 			<p>No student pays for tuition.</p>
 
-			<p>Our generous <strong>TUITION PROMISE SCHOLARSHIP</strong> makes it possible for you to graduate debt-free. Even if you borrow for special learning opportunities or to replace your family’s total contribution, you will have a low debt compared to national trends. We sometimes call Berea “the best education money can’t buy.”</p>
+			<p>Our generous <strong class="text-uppercase">Tuition promise sholarship</strong> makes it possible for you to graduate debt-free. Even if you borrow for special learning opportunities or to replace your family’s total contribution, you will have a low debt compared to national trends. We sometimes call Berea “the best education money can’t buy.”</p>
 		</div><!-- .footer-promise -->
 
 		<div class="footer-about">
@@ -47,7 +47,7 @@
 				<li><a href="http://www.kentuckytourism.com/">Kentucky</a></li>
 			</ul>
 
-			<h3>INITIATIVES</h3>
+			<h3 class="text-uppercase">Initiatives</h3>
 
 			<ul>
 				<li><a href="https://www.berea.edu/brushy-fork-institute/">Brushy Fork Institute</a></li>
@@ -63,15 +63,15 @@
 		<div class="footer-distinctly">
 			<h3 class="element-uppercase">Distinctly Berean</h3>
 
-				<a href="https://www.berea.edu/visitor-center/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/visitorcenter165x59.png" alt="Visitor Center &amp; Shoppe"/></a>
+				<a href="https://www.berea.edu/visitor-center/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/visitorcenter165x59.png" aria-label="Visitor Center &amp; Shoppe"/></a>
 				<br><br><br>
-				<a href="http://www.boonetavernhotel.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/boonetavern165x59.png" alt="Boone Tavern"/></a>
+				<a href="http://www.boonetavernhotel.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/boonetavern165x59.png" aria-label="Boone Tavern"/></a>
 				<br><br><br>
-				<a href="http://www.bereacrafts.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/studentcrafts165x42.png" alt="Student Crafts"/></a>
+				<a href="http://www.bereacrafts.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/studentcrafts165x42.png" aria-label="Student Crafts"/></a>
 				<br><br><br>
-				<a href="https://www.berea.edu/conference-services/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/conference-services-165x42.png" alt="Conference Services"/></a>
+				<a href="https://www.berea.edu/conference-services/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/conference-services-165x42.png" aria-label="Conference Services"/></a>
 				<br><br><br>
-				<a href="http://bereacollegefarmstore.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/farm_store.png" alt="Farm Store"/></a>
+				<a href="http://bereacollegefarmstore.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/farm_store.png" aria-label="Farm Store"/></a>
 
 		</div><!-- .footer-distinctly -->
 
@@ -87,7 +87,7 @@
                 </div>
 
                 <div id="berea_logo_leftside">
-                    <a href="https://www.berea.edu/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bc-logo-stacked.svg" alt="Berea College"/></a>
+                    <a href="https://www.berea.edu/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bc-logo-stacked.svg" aria-label="Berea College"/></a>
                 </div>
 
             </div><!-- #topleft -->
@@ -95,7 +95,7 @@
             <div id="bottomright">
 
                 <div id="black-text">
-                    <a href="https://www.google.com/maps/place/Berea+College/@37.5716935,-84.2890655,17z/data=!4m2!3m1!1s0x8842e192a683f201:0x193cd1402103123a"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/map-image.png" alt="Map to Berea College"/></a></a>
+                    <a href="https://www.google.com/maps/place/Berea+College/@37.5716935,-84.2890655,17z/data=!4m2!3m1!1s0x8842e192a683f201:0x193cd1402103123a"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/map-image.png" aria-label="Map to Berea College"/></a></a>
                 </div>
 
                 <div id="gmap_links_undermap">
