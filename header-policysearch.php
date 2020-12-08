6bb803861eeecb08ab76347509bf7b35d1cbcb77
Dan Adams <danadams1278@gmail.com>
2020-12-08 15:41:05 -0500

    facebook pixel

--- header-policysearch.php
+++ header-policysearch.php
@@ -53,6 +53,26 @@ if ( is_front_page() && wp_get_theme()->name == 'Berea 2015 - Main Site Child Th
   }}();
 </script>
 
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '308581360085575');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=308581360085575&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+
+
 
 
 
